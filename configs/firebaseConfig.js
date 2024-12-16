a95715ec4f83d1028bb2bbfeddaac8011d07e855
IIIgorMoura <133662971+IIIgorMoura@users.noreply.github.com>
2024-12-16 17:32:40 -0300

    safChange

--- configs/firebaseConfig.js
+++ configs/firebaseConfig.js
@@ -2,14 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDn4aFWKEk_j5_SvgTqM88dO9e4iBGYPQc",
-    authDomain: "rh-integrity.firebaseapp.com",
-    projectId: "rh-integrity",
-    storageBucket: "rh-integrity.firebasestorage.app",
-    messagingSenderId: "734916290985",
-    appId: "1:734916290985:web:e3c668d851cedfb7827cb8"
-  };
+// API aqui
 
 const app = initializeApp(firebaseConfig);
 
